e61fdac6fe4d2f645f1174e60a8cb54ade201c80
audiofeature <reblaus15>
2015-06-19 11:01:32 +0200

    rewrote getSimilarSongs as function, putting rest into main function

--- getSimilarSongs.py
+++ getSimilarSongs.py
@@ -12,56 +12,23 @@ from audiofile_read import *  # included in the rp_extract git package
 from rp_extract_files import read_feature_files
 from rp_extract_python import rp_extract
 
-
 # for Similarity Search import
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 
 
-# REFERENCE DATA SET
+# getSimilarSongs:
 
-# 0) We pre-analyzed a set of MP3 files to store the features in a CSV file
-
-# using rp_extract_files.py
-# editing the in_path and out_path in the __main__ function
-
-# set the same path here as used in rp_extract_files.py
-MP3_PATH = "/Users/jjb/Music/mp3/1972/"
-
-
-
-# 1) Load pre-analysed features from csv: pre_features
-
-# set feature type
-feature_type = 'rh' # rhythmic: rp, rh   more timbral: ssd
-
-feature_sets = [feature_type] # here we set the same, but this usually is an array of multiple feature sets [.]
-
-filenamestub = 'features'
-
-# read CSV: get the ids and features
-# our ids are the mp3_filenames (relative path only)
-mp3_filenames, db_features = read_feature_files(filenamestub,feature_sets)
-
-
-
-
-
-audiofile = "./generated.wav"
-
-
-# new_wavfile: name of .wav file to be analyzed as new query
+# audiofile: name of .wav (or .mp3) file to be analyzed as new query
 # reference_db_filenames: list of audio filenames that were pre-analyzed
 # reference_db_features: a dict containing the pre-analyzed features from the reference_db
 # feature_type: feature type to be used for similarity search: 'rh' or 'rp' or 'ssd' ...
 
-
-
-def getSimilarSongs(new_wavfile, reference_db_filenames, reference_db_features, feature_type = 'rh'):
+def getSimilarSongs(audiofile, reference_db_filenames, reference_db_features, feature_type = 'rh'):
 
     # Read Wav File
 
-    samplerate, samplewidth, wavedata = audiofile_read(new_wavfile)
+    samplerate, samplewidth, wavedata = audiofile_read(audiofile)
 
     # Analyze New Audio
 
@@ -113,84 +80,140 @@ def getSimilarSongs(new_wavfile, reference_db_filenames, reference_db_features, 
 
     most_similar_songs = reference_db_filenames[feature_type][similar_song_ids]
 
+    # most_similar_songs is a 2D np.array, make it 1D
+    most_similar_songs = most_similar_songs[0]
+
+    # transform from np.array to simple list
+    most_similar_songs = most_similar_songs.tolist()
+
+    return(most_similar_songs)
+
+
+
+# defGetCenteredSegment(...):
+# THIS IS TEST CODE, NOT YET FINISHED
+    # 5) get 30 seconds around the middle timestamp
+
+    # output_length = 30 # duration of desired output segment in seconds
+    #
+    # middle_timestamp = (most_similar_timestamp[0] + most_similar_timestamp[1]) / 2
+    #
+    # print middle_timestamp
+    #
+    # start_pos = middle_timestamp - (output_length / 2)
+    # if start_pos < 0:
+    #     start_pos = 0
+    #
+    # end_pos = start_pos + output_length
+    # # TODO:
+    # # if end_pos > duration of song
+    #     # end pos = duration of song
+    #     # start_pos = end_pos - output_length
+    # # TODO: MAYBE ITS BETTER to do everything in samples (segpos) instead of timepos
+    #
+    # print "Output segment:", start_pos, end_pos
+
+
+
+
+if __name__ == '__main__':
+
+
+    # REFERENCE DATA SET
+
+    # 0) We pre-analyzed a set of MP3 files to store the features in a CSV file
+
+    # using rp_extract_files.py
+    # editing the in_path and out_path in the __main__ function
+
+    # set the same path here as used in rp_extract_files.py
+    MP3_PATH = "/Users/jjb/Music/mp3/1972/"
+
+
+    # 1) Load pre-analysed features from csv: pre_features
+
+    # set feature type
+    feature_type = 'rh' # rhythmic: rp, rh   more timbral: ssd
+
+    feature_sets = [feature_type] # here we set the same, but this usually is an array of multiple feature sets [.]
+
+    filenamestub = 'features'
+
+    # read CSV: get the ids and features
+    # our ids are the mp3_filenames (relative path only)
+    mp3_filenames, reference_db_filenames = read_feature_files(filenamestub,feature_sets)
+
+
+
+    # get similar songs with new wav file:
+
+    new_wavfile = "./generated.wav"
+
+    # TOM TEST
+    new_wavfile = "../rp_extract/music/24bit_audio.wav"
+
+    similar_songs = getSimilarSongs(new_wavfile, mp3_filenames, reference_db_filenames, feature_type)
+
     print "Most similar songs:"
 
-    print most_similar_songs
+    print similar_songs
+
+    print type(similar_songs)
 
 
+    # 4) Now for the most similar song (1 or 2 or 3) we analyze the FULL song again
+    #    and find the BEST SEGMENT that matches the original query
+
+    song_n = 0
+
+    audiofile = MP3_PATH + similar_songs[song_n]
+
+    print audiofile
+
+    # TOM TEST
+    audiofile = "../rp_extract/music/Remy Boyz - My Way RMX Ft. Drake.mp3"
 
 
+    samplerate, samplewidth, wavedata = audiofile_read(audiofile)
 
 
-# 4) Now for the most similar song (1 or 2 or 3) we analyze the FULL song again
-#    and find the BEST SEGMENT that matches the original query
+    fext = feature_sets = [feature_type]
 
-song_n = 0
+    segment_features = rp_extract(wavedata,
+                      samplerate,
+                      extract_rp   = ('rp' in fext),          # extract Rhythm Patterns features
+                      extract_ssd  = ('ssd' in fext),           # extract Statistical Spectrum Descriptor
+                      extract_sh   = ('sh' in fext),          # extract Statistical Histograms
+                      extract_tssd = ('tssd' in fext),          # extract temporal Statistical Spectrum Descriptor
+                      extract_rh   = ('rh' in fext),           # extract Rhythm Histogram features
+                      extract_trh  = ('trh' in fext),          # extract temporal Rhythm Histogram features
+                      extract_mvd  = ('mvd' in fext),        # extract Modulation Frequency Variance Descriptor
+                      skip_leadin_fadeout=0,
+                      step_width=1,
+                      return_segment_features = True)
 
-audiofile = MP3_PATH + most_similar_songs[0,song_n]
+    search_features = segment_features[feature_type]
 
-print audiofile
+    sim_song_search = NearestNeighbors(n_neighbors = 1, metric='euclidean')
 
+    # TODO proper Scaling (Normalization) (see above)
 
-#samplerate, samplewidth, wavedata = audiofile_read(audiofile)
+    sim_song_search.fit(search_features)
 
-segment_features = rp_extract(wavedata,
-                  samplerate,
-                  extract_rp   = ('rp' in fext),          # extract Rhythm Patterns features
-                  extract_ssd  = ('ssd' in fext),           # extract Statistical Spectrum Descriptor
-                  extract_sh   = ('sh' in fext),          # extract Statistical Histograms
-                  extract_tssd = ('tssd' in fext),          # extract temporal Statistical Spectrum Descriptor
-                  extract_rh   = ('rh' in fext),           # extract Rhythm Histogram features
-                  extract_trh  = ('trh' in fext),          # extract temporal Rhythm Histogram features
-                  extract_mvd  = ('mvd' in fext),        # extract Modulation Frequency Variance Descriptor
-                  skip_leadin_fadeout=0,
-                  step_width=1,
-                  return_segment_features = True)
+    # Get the most similar songs
+    most_similar_segment_index = sim_song_search.kneighbors(query_feature_vector, return_distance=False)
 
-search_features = segment_features[feature_type]
+    # here you get the segment's sample position
+    # segment_features['segpos']
 
-sim_song_search = NearestNeighbors(n_neighbors = 1, metric='euclidean')
+    # and time positions in seconds
+    # segment_features['timepos']
 
-# TODO proper Scaling (Normalization) (see above)
+    most_similar_timestamp = segment_features['timepos'][most_similar_segment_index]
 
-sim_song_search.fit(search_features)
+    # quit the [[[]]] (we got a nested array for some reason
+    most_similar_timestamp = most_similar_timestamp[0][0]
 
-# Get the most similar songs
-most_similar_segment_index = sim_song_search.kneighbors(query_feature_vector, return_distance=False)
+    print most_similar_timestamp
 
-# here you get the segment's sample position
-# segment_features['segpos']
-
-# and time positions in seconds
-# segment_features['timepos']
-
-most_similar_timestamp = segment_features['timepos'][most_similar_segment_index]
-
-# quit the [[[]]] (we got a nested array for some reason
-most_similar_timestamp = most_similar_timestamp[0][0]
-
-print most_similar_timestamp
-
-start_pos = most_similar_timestamp[0]
-
-# 5) get 30 seconds around the middle timestamp
-
-# output_length = 30 # duration of desired output segment in seconds
-#
-# middle_timestamp = (most_similar_timestamp[0] + most_similar_timestamp[1]) / 2
-#
-# print middle_timestamp
-#
-# start_pos = middle_timestamp - (output_length / 2)
-# if start_pos < 0:
-#     start_pos = 0
-#
-# end_pos = start_pos + output_length
-# # TODO:
-# # if end_pos > duration of song
-#     # end pos = duration of song
-#     # start_pos = end_pos - output_length
-# # TODO: MAYBE ITS BETTER to do everything in samples (segpos) instead of timepos
-#
-# print "Output segment:", start_pos, end_pos
-
+    start_pos = most_similar_timestamp[0]
